09eaa3ac9d9c2a629353110e6a5557ef42c32794
ozcansayis <ozcansayis@gmail.com>
2024-05-24 01:15:24 +0300

    if disabled

--- src/test/java/StepDefinitions/Hooks.java
+++ src/test/java/StepDefinitions/Hooks.java
@@ -9,11 +9,11 @@ import org.openqa.selenium.TakesScreenshot;
 public class Hooks {
     @After
     public void after(Scenario senaryo) {
-        if (senaryo.isFailed()) {
-            TakesScreenshot ts = (TakesScreenshot) GWD.getDriver();
-            byte[] hafizadakiHali = ts.getScreenshotAs(OutputType.BYTES);
-            senaryo.attach(hafizadakiHali, "image/png", "screenshot name");
-        }
+//        if (senaryo.isFailed()) {
+//            TakesScreenshot ts = (TakesScreenshot) GWD.getDriver();
+//            byte[] hafizadakiHali = ts.getScreenshotAs(OutputType.BYTES);
+//            senaryo.attach(hafizadakiHali, "image/png", "screenshot name");
+//        }
 
         GWD.quitDriver();
     }
