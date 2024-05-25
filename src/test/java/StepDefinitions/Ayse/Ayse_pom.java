2b1aa35726ee841f6e3a0efa43253296b534de0f
Ayse Ihtiyar <ayseeihtiyar@gmail.com>
2024-05-26 01:55:29 +0300

    US_008 is done.

--- src/test/java/StepDefinitions/Ayse/Ayse_pom.java
+++ src/test/java/StepDefinitions/Ayse/Ayse_pom.java
@@ -1,4 +1,43 @@
 package StepDefinitions.Ayse;
 
-public class Ayse_pom {
+import Pages.ParentPage;
+import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.*;
+
+public class Ayse_pom extends ParentPage {
+
+    public Ayse_pom() {
+        PageFactory.initElements(GWD.getDriver(),this);
+    }
+
+    @FindBy(css = "[formcontrolname='username']")
+    public WebElement username;
+
+    @FindBy(css = "[formcontrolname='password']")
+    public WebElement password;
+
+
+    @FindBy(className = "mdc-button__label")
+    public WebElement loginButton;
+
+    // Hamburger Menu
+
+    @FindBy(css = "[aria-haspopup='menu']")
+    public WebElement hamburgerMenu;
+
+    @FindBy(xpath = "//div[@class='cdk-overlay-container']/div[2]//button[2]")
+    public WebElement finance;
+
+    @FindBy(xpath = "//div[@class='cdk-overlay-container']/div[3]//button")
+    public WebElement myFinance;
+
+    //my finance
+
+    @FindBy(css = "thead[class='ng-star-inserted']+tbody")
+    public WebElement userInformation;
+
+    @FindBy(css = "thead[class='ng-star-inserted']+tbody td:nth-child(10) button")
+    public WebElement viewButton;
+
 }
