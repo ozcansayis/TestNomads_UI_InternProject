28659f04704ff473d92d565236d8577b88eeea83
umiteser <eser_umit@hotmail.com>
2024-05-30 16:30:02 +0300

    _016_Steps completed

--- src/test/java/StepDefinitions/Umit/_016_Steps.java
+++ src/test/java/StepDefinitions/Umit/_016_Steps.java
@@ -1,32 +1,42 @@
 package StepDefinitions.Umit;
 
 import Pages.ParentPage;
+import Utilities.GWD;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.JavascriptExecutor;
 
 import java.awt.*;
 
 public class _016_Steps extends ParentPage {
 
     Umit_pom um = new Umit_pom();
+    JavascriptExecutor js = (JavascriptExecutor) GWD.getDriver();
 
     @When("the user clicks grading")
     public void theUserClicksGrading() {
         myClick(um.grading);
     }
+
     @Then("the user clicks course grade")
     public void theUserClicksCourseGrade() {
-        myClick(um.courseGrade);
+        WaitNano(10);
+        js.executeScript("arguments[0].click();", um.courseGrade);
+
     }
 
     @And("the user clicks student transcript")
     public void theUserClicksStudentTranscript() {
-        myClick(um.studentTranscript);
+        WaitNano(10);
+        js.executeScript("arguments[0].click();", um.studentTranscript);
+
     }
 
     @When("the user clicks transcript by subject")
     public void theUserClicksTranscriptBySubject() {
-        myClick(um.transcriptBySubject);
+        WaitNano(10);
+        js.executeScript("arguments[0].click();", um.transcriptBySubject);
+
     }
 }
