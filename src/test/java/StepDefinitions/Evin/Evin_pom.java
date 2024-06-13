df674b3f48cd5acaabab6d3795b19232512d355f
evinozeler <evin.ertases@gmail.com>
2024-06-13 11:24:17 -0700

    002 completed

--- src/test/java/StepDefinitions/Evin/Evin_pom.java
+++ src/test/java/StepDefinitions/Evin/Evin_pom.java
@@ -1,4 +1,27 @@
 package StepDefinitions.Evin;
 
-public class Evin_pom {
+import Pages.ParentPage;
+import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Evin_pom extends ParentPage {
+    public Evin_pom() {PageFactory.initElements(GWD.getDriver(), this);
+    }
+    //login
+    @FindBy(css = "[placeholder='Kullanıcı Adı']")
+    public WebElement username;
+
+    @FindBy(css = "[placeholder='Parola']")
+    public WebElement password;
+
+    @FindBy(css = "[aria-label='LOGIN']")
+    public WebElement loginButton;
+    @FindBy(css = "mat-toolbar div > img")
+    public WebElement logo;
+
+    @FindBy (css = "[class='tn-atom js-click-zero-stat']")
+    public WebElement homepage;
+
 }
